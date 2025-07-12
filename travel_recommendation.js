fc364ca7fdd532e2fa31071941e320f7d043fcc7
Tshegofatso85 <tshegofatsokgokong19@gmail.com>
2025-07-11 20:35:45 -0400

    results found added

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,4 +1,5 @@
     function performSearch() {
+      resetSearchResults();
       const input = document.getElementById("searchInput").value;
       const searchResults = document.querySelector('.search-results')
 
@@ -11,11 +12,11 @@
       .then(data => {
 
         searchResults.style.display = "block"
-
+        let newData = []
         const keysAvail = Object.keys(data).find(ele => ele.startsWith(input.trim().toLowerCase()));
 
         if (!keysAvail) {
-            const searchResults = document.querySelector('.search-results');
+            // const searchResults = document.querySelector('.search-results');
 
             const resultDiv = document.createElement('div');
             resultDiv.classList.add('each-result');
@@ -25,10 +26,37 @@
 
             resultDiv.appendChild(message);
             searchResults.appendChild(resultDiv);
+        } else {
+            if (keysAvail === "countries"){
+                newData = data[keysAvail].flatMap(country => country.cities)
+            } else {
+                newData = data[keysAvail]
+            }
+
+            newData.forEach(item => {
+                const resultDiv = document.createElement('div');
+                resultDiv.classList.add('each-result');
+                resultDiv.innerHTML = `
+                  <img src="${item.image}" alt="${item.name}">
+                  <h2>${item.name}</h2>
+                  <p>${item.description}</p>
+                  <button>view</button>
+                `;
+                searchResults.appendChild(resultDiv);
+            });
         }
       })
     }
 
     function resetSearch() {
       document.getElementById("searchInput").value = "";
+      resetSearchResults();
     }
+
+    function resetSearchResults() {
+        const searchResults = document.querySelector('.search-results');
+        if (searchResults) {
+          searchResults.innerHTML = ''; // Clears previous results
+          searchResults.style.display = "none";
+        }
+      }
